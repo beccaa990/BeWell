c21d045b1d413a77a72770378c52d6adce81e6ba
Rebecca Ashleigh <AshleighR@liverpool.gov.uk>
2024-05-30 15:48:23 +0100

    adding summary to the navigation bar

---
+++ Rebecca.BeWell.BlazorApp.Shared/Models/AreaSeriesItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rebecca.BeWell.BlazorApp.Shared.Models
+{
+    public class AreaSeriesItem
+    {
+        public DateOnly Date { get; set; }
+
+        public int Mins { get; set; }
+
+        public string Intensity { get; set; }
+
+        public string Type { get; set; }
+
+
+    }
+}
---
+++ Rebecca.BeWell.BlazorApp/Rebecca.BeWell.BlazorApp.Client/Pages/Summary.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+using Radzen;
+using Radzen.Blazor.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using static System.Net.WebRequestMethods;
+using System.Net.Http.Json;
+using Rebecca.BeWell.BlazorApp.Shared.Models;
+
+
+
+namespace Rebecca.BeWell.BlazorApp.Client.Pages
+{
+    public partial class Summary : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+        public List<AreaSeriesItem> ActivityData { get; set; }
+        public List<Shared.Data.Models.ActivityType> ActivityTypes { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Rebecca.BeWell.BlazorApp.Shared.Data.Models.Profile profile = await Http.GetFromJsonAsync<Shared.Data.Models.Profile>("api/Profiles/user/current");
+
+            ActivityTypes = await  Http.GetFromJsonAsync<List<Shared.Data.Models.ActivityType>>("api/Activities/GetActivityTypes");
+
+            ActivityData = profile.Activities.Select(a => new Shared.Models.AreaSeriesItem()
+            {
+                Date = DateOnly.FromDateTime( a.Start),
+                Mins = a.Mins,
+                Intensity = a.Intensity.Name,
+                Type = a.ActivityType.Name,
+
+            }).ToList();
+        }
+    }
+}
